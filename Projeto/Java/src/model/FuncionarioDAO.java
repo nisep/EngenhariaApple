97813a4885d14dda63145236a66bf068a5b95d99
carlosjacinto <carlosjacinto@gec.inatel.br>
2017-09-26 14:44:10 -0300

    Alterado 26-09

--- Projeto/Java/src/model/FuncionarioDAO.java
+++ Projeto/Java/src/model/FuncionarioDAO.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -22,6 +26,9 @@ public class FuncionarioDAO {
 							+ f.getDataAdmissao() + "','" + f.getCep() + "','" + f.getSenha() + "','" + f.getFoto()
 							+ "','" + f.getTelefone() + "','" + f.getCelular() + "','" + f.getCpf() + "','"
 							+ f.getSalario() + "','" + f.getComissao() + "') ");
+			
+			int codigo = buscaCodigoFuncionario(f.getCpf());
+			CopiarImagemFuncionario(codigo,f.getFoto());
 			System.out.println("deu bom");
 			return true;
 		} catch (SQLException sqle) {
@@ -33,22 +40,34 @@ public class FuncionarioDAO {
 
 	}
 
+	private int buscaCodigoFuncionario(long CPF) {
+		conex = bd.Conectar();
+		try {
+			Statement stmt = (Statement) conex.createStatement();
+			String SQL = "SELECT * FROM funcionario";
+			ResultSet rs = stmt.executeQuery(SQL);
+			double cpfF;
+			while (rs.next()) {
+				cpfF = rs.getDouble("cpfFunc");
+
+				if (cpfF == CPF) {
+					return rs.getInt("idFuncionario");
+				}
+			}
+		} catch (SQLException sqle) {
+			System.out.println("Erro ao consultar..." + sqle.getMessage());
+			return 0;
+		} finally {
+			bd.Desconectar(conex);
+		}
+		return 0;
+	}
+
 	public boolean verificaCPF(long CPF) {
 		conex = bd.Conectar();
 		try {
-<<<<<<< HEAD
-			Statement stmt = conex.createStatement();
-			stmt.execute("select * from funcionario where ");
-			return true;
-		}catch(SQLException sqle) {
-			System.out.println(sqle.getMessage());
-			return false;
-		}finally {
-			bd.Desconectar(conex);
-		}
-=======
 			Statement stmt = (Statement) conex.createStatement();
-			String SQL = "SELECT * FROM veiculo";
+			String SQL = "SELECT * FROM funcionario";
 			ResultSet rs = stmt.executeQuery(SQL);
 			double cpfF;
 			while (rs.next()) {
@@ -65,7 +84,28 @@ public class FuncionarioDAO {
 			bd.Desconectar(conex);
 		}
 		return false;
+	}
 
->>>>>>> 2c90b2534c92fd3917afc995cf70aad5429b8d0e
+	public void CopiarImagemFuncionario(int codigo, String caminho) {
+		FileInputStream origem= null;
+		FileOutputStream destino = null;
+		FileChannel fcOrigem = null;
+		FileChannel fcDestino = null;
+		try {
+			origem = new FileInputStream(caminho);
+			System.out.println();
+			destino = new FileOutputStream("/Media/Funcionario/" + codigo + ".jpg");
+			fcOrigem = origem.getChannel();
+			fcDestino = destino.getChannel();
+			fcOrigem.transferTo(0, fcOrigem.size(), fcDestino);
+			origem.close();
+			destino.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		
+	
+
 	}
 }
